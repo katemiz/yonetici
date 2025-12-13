48639110d75b2f85b01e07b23fd5db6b652f8424
Kılıç Ali Temiz <katemiz@gmail.com>
2025-12-13 20:44:50 +0300

    guest-layout is missing

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+<x-log-layout>
     <x-auth-card>
         <x-slot name="logo">
             {{-- <a href="/">
@@ -31,4 +31,4 @@
             </div>
         </form>
     </x-auth-card>
-</x-guest-layout>
+</x-log-layout>
